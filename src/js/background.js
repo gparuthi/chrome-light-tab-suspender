b2938e5bf10d8a99e5990b80bef632f7eebe41cd
Paruthi, Gaurav <1958947+gparuthi@users.noreply.github.com>
2024-02-12 13:20:37 -0800

    fix

--- src/js/background.js
+++ src/js/background.js
@@ -9,39 +9,91 @@ chrome.commands.onCommand.addListener((command) => {
 });
 
 
-let tabTimes = {};
-const INACTIVE_LIMIT = 60 * 1000; // 1 sec
+const INACTIVE_LIMIT = 1 * 1000; // 1 sec
 
-function checkInactiveTabs() {
-    let inactiveLimit = INACTIVE_LIMIT; // 1 minutes
-    let now = Date.now();
-
-    chrome.tabs.query({ }, (tabs) => {
-      for (let tab of tabs) {
-
-        if (tab.url.includes('keep.google.com') && !tab.url.includes("chrome-extension")) {
-          checkInactiveTab(tab)
-        }
-      }
-    }
-    );
+let d = {
+  tab: null,
+  time: null
 }
 
+async function checkInactiveTabs() {
+  keeptab = await getKeepTab();
+  if (keeptab) {
+    d.tab = keeptab;
+    checkInactiveTab(d.tab)
+  }
+  
+}
+
+function getKeepTab() {
+  return new Promise((resolve) => {
+    chrome.tabs.query({}, (tabs) => {
+      let keepTab = null;
+      for (let tab of tabs) {
+        if (isKeepTab(tab)) {
+          keepTab = tab;
+          break;
+        }
+      }
+      resolve(keepTab);
+    });
+  });
+}
+
+chrome.tabs.onActivated.addListener(async activeInfo => {
+  if (!d.tab){
+    return;
+  }
+    if (String(d.tab.id) === String(activeInfo.tabId)) {
+      unsuspend(d.tab);
+    }
+});
+
+chrome.windows.onFocusChanged.addListener(function(windowId) {
+  console.log(d)
+  if (!d.tab) {
+    return;
+  }
+
+  console.log(windowId, d.tab.windowId, d.tab.windowId === windowId)
+  
+
+  if (windowId === chrome.windows.WINDOW_ID_NONE) {
+    suspend(d.tab);
+  }
+  if (windowId === d.tab.windowId) {
+    unsuspend(d.tab);
+  }
+});
+
+function isKeepTab(tab) {
+  return tab.url.includes('keep.google.com') && !tab.url.includes("chrome-extension")
+}
 
 function checkInactiveTab(tab) {
   if (tab.active === true) {
-    tabTimes[tab.id] = Date.now();
+    d.time = Date.now();
   } else {
     let now = Date.now();
-    let time = tabTimes[tab.id];
+    let time = d.time;
     let inactiveLimit = INACTIVE_LIMIT;
 
     if (now - time > inactiveLimit) {
       console.log(`Tab ${tab.url} is inactive for ${now - time} ms.`);
-      suspend.suspend(tab)
+      suspend(tab)
     }
   
   }
+}
+
+function suspend(tab) {
+  const suspendUrl = `chrome-extension://${chrome.runtime.id}/tab.html`;
+  chrome.tabs.update(tab.id, { url: `${suspendUrl}#url=${tab.url}` });
+}
+
+function unsuspend(tab) {
+  const url = "https://keep.google.com/"
+  chrome.tabs.update(tab.id, { url });
 }
 
 // Check inactive tabs every minute
